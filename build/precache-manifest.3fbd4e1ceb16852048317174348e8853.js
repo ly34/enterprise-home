26cda579b0bb8d255bde931c7d3c270dc77e02f9
wayou <liuwayong@gmail.com>
2020-10-17 22:50:55 +0800

    update build artifical

---
+++ build/precache-manifest.3fbd4e1ceb16852048317174348e8853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa44575bfa4a551fd81261be7a65e91b",
+    "url": "https://ly34.github.io/enterprise-home/index.html"
+  },
+  {
+    "revision": "67374b24a024738687f5",
+    "url": "https://ly34.github.io/enterprise-home/static/css/main.00965c0d.chunk.css"
+  },
+  {
+    "revision": "23a08cb856adc82af8ca",
+    "url": "https://ly34.github.io/enterprise-home/static/js/2.06614054.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "https://ly34.github.io/enterprise-home/static/js/2.06614054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67374b24a024738687f5",
+    "url": "https://ly34.github.io/enterprise-home/static/js/main.ad1f9554.chunk.js"
+  },
+  {
+    "revision": "c0caf05c0ecfe009e9e7",
+    "url": "https://ly34.github.io/enterprise-home/static/js/runtime-main.15742aca.js"
+  },
+  {
+    "revision": "19d351fe572248bb676a7269a2f2f06d",
+    "url": "https://ly34.github.io/enterprise-home/static/media/logo.19d351fe.png"
+  }
+]);
